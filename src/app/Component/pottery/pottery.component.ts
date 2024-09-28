81de6fbe8cd0f1a03da33e8b19d67835fb929198
omniaabuelhassan <omnya200489@gmail.com>
2024-09-28 19:07:54 +0300

    upload pottery

---
+++ src/app/Component/pottery/pottery.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pottery',
+  templateUrl: './pottery.component.html',
+  styleUrl: './pottery.component.scss'
+})
+export class PotteryComponent {
+
+}
