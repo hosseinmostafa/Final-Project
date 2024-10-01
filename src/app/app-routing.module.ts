8f0bdd54827d7cc96f57d808f3d3c39bc0c8ac77
hosseinmostafa <hesseinnmostafaa@gmail.com>
2024-10-01 10:46:54 +0300

    Updated styles and added new colors to the application, modified footer and payment components, and made changes to the index.html file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { HeaderComponent } from './Component/header/header.component';
 import { FooterComponent } from './Component/footer/footer.component';
 import { PotteryComponent } from './Component/pottery/pottery.component';
 import { NotFondComponent } from './Component/notFond/not-fond/not-fond.component';
+import { PaymentComponent } from './Component/payment/payment.component';
 
 const routes: Routes = [
   {path: "", component : HomeComponent},
   {path: "home", component : HomeComponent},
   {path: "sgin-in-sign-up", component : SginInSignUpComponent},
   {path: "pottery", component : PotteryComponent},
+  {path: "payment", component : PaymentComponent},
   {path: "**", component : NotFondComponent}
 ];
 
