97b0f681571154f70f91a81ed56e702584a77851
osama sameh <oo215881@gmail.com>
2024-10-01 20:24:48 +0300

    home page

--- src/app/Component/header/header.component.ts
+++ src/app/Component/header/header.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  loginTextVisible = false; // Determines whether the login text is shown
+  loginIconOpacity = 1; // Controls the opacity of the icon
 
+  // Method to show the login text and reduce the icon opacity
+  showLoginText(): void {
+    this.loginTextVisible = true;
+    this.loginIconOpacity = 0.5;
+
+  }
+
+  // Method to hide the login text and reset the icon opacity
+  hideLoginText(): void {
+    this.loginTextVisible = false;
+    this.loginIconOpacity = 1;
+  }
 }
--- src/app/Component/sgin-in-sign-up/sgin-in-sign-up.component.ts
+++ src/app/Component/sgin-in-sign-up/sgin-in-sign-up.component.ts
@@ -1,4 +1,9 @@
 import { Component, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ForbiddenNameValidator } from '../../CostmorFormSigin/costemFormUserName';
+import { ConfirmPasswordValidator } from '../../CostmorFormSigin/costemFormPassword';
+import { UserService } from '../../Services/user.service';
+import { USERModul } from './UserModule';
 
 @Component({
   selector: 'app-sgin-in-sign-up',
@@ -9,9 +14,24 @@ export class SginInSignUpComponent {
 
   img1: string = "./img-Sign/add-to-cart-animate (3).svg"
   img2: string = "./img-Sign/online-shopping-animate.svg"
+  
+  regsetForm: FormGroup
 
-  constructor(private elementRef: ElementRef) { }
+  userModel = new USERModul('', '', '', '', false);
+  constructor(private elementRef: ElementRef, private fb: FormBuilder, private userService: UserService) {
+    this.regsetForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(3), ForbiddenNameValidator]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+      location: this.fb.group({
+        city: [''],
+        state: [''],
+        postalCode: ['']
+      })
+    }, { validators: [ConfirmPasswordValidator] })
+  }
 
+  // ---------------------------
   ngAfterViewInit(): void {
     const sign_in_btn = this.elementRef.nativeElement.querySelector('#sign-in-btn');
     const sign_up_btn = this.elementRef.nativeElement.querySelector('#sign-up-btn');
@@ -20,24 +40,41 @@ export class SginInSignUpComponent {
     const sign_in_btn2 = this.elementRef.nativeElement.querySelector('#sign-in-btn2');
     const sign_up_btn2 = this.elementRef.nativeElement.querySelector('#sign-up-btn2');
 
-    // Adding 'sign-up-mode' when sign-up button is clicked
     sign_up_btn.addEventListener('click', () => {
       container.classList.add('sign-up-mode');
     });
 
-    // Removing 'sign-up-mode' when sign-in button is clicked
     sign_in_btn.addEventListener('click', () => {
       container.classList.remove('sign-up-mode');
     });
 
-    // Adding 'sign-up-mode2' when second sign-up button is clicked
     sign_up_btn2.addEventListener('click', () => {
       container.classList.add('sign-up-mode2');
     });
 
-    // Removing 'sign-up-mode2' when second sign-in button is clicked
     sign_in_btn2.addEventListener('click', () => {
       container.classList.remove('sign-up-mode2');
     });
   }
+  // ------------------
+  gitData() {
+    this.regsetForm.patchValue({
+      userName: 'Ahmed',
+      location: {
+        city: 'Egypt',
+        state: 'Hurghada',
+        postalCode: '123456'
+      }
+    })
+
+  }
+
+  onSubmit() {
+    this.userService.addUser(this.userModel).subscribe({
+      next: (data) => console.log(data),
+      error: (error) => console.log(error),
+    });
+  }
+
+
 }
