bc287167159438fe52f4ba10f8a3f8a931821f45
Hussein-osama <so7hs.oh@gmail.com>
2024-10-02 23:03:55 +0300

    header done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { HomeComponent } from './Component/home/home.component';
 import { SginInSignUpComponent } from './Component/sgin-in-sign-up/sgin-in-sign-up.component';
 import { HeaderComponent } from './Component/header/header.component';
 import { FooterComponent } from './Component/footer/footer.component';
-import { PotteryComponent } from './Component/pottery/pottery.component';
 import { NotFondComponent } from './Component/notFond/not-fond/not-fond.component';
 import { PaymentComponent } from './Component/payment/payment.component';
 import { AddProductComponent } from './Component/add-product/add-product.component';
@@ -13,7 +12,6 @@ const routes: Routes = [
   {path: "", component : HomeComponent},
   {path: "home", component : HomeComponent},
   {path: "sgin-in-sign-up", component : SginInSignUpComponent},
-  {path: "pottery", component : PotteryComponent},
   {path: "payment", component : PaymentComponent},
    {path: "add-prouduct", component : AddProductComponent},
   {path: "**", component : NotFondComponent}
