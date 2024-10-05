5a6a3fc932872d7a0bc772bd9b25f1e35e3e7d34
omniaabuelhassan <omnya200489@gmail.com>
2024-10-05 06:33:16 +0300

    connect product details page with product

--- src/app/Component/product-details/product-details.component.ts
+++ src/app/Component/product-details/product-details.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../Services/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Iproduct } from '../interfaces/Iproduct';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './product-details.component.scss'
 })
 export class ProductDetailsComponent {
+  oneProduct: any;
+  productId:any;
+errMsg: any;
+constructor(private productService: ProductService , private activatedRoute:ActivatedRoute ) {}
 
+ngOnInit(): void {
+  this.productId = this.activatedRoute.snapshot.paramMap.get('id') ;
+  this.productService.getOneProduct(this.productId).subscribe({
+    next: (data) => {
+      this.oneProduct = data;
+      console.log(this.oneProduct);
+    },
+    error: (err) => {
+      return throwError(() => err.message || 'product not found');
+    }
+})
+}
 }
